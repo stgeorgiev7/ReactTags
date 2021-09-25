94d3dc6c5999d042a15f099b5aa8aedb2190011c
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-09-25 17:30:45 +0300

    hardcode

--- src/Tags.js
+++ src/Tags.js
@@ -4,12 +4,14 @@ const Tags = (tags) => {
     const arr = tags;
     
     return (<div className='tags'>
-         <title className='tag'>#{arr.shift()}</title>
-         <title className='tag'>#{arr.shift()}</title>
-         <title className='tag'>#{arr.shift()}</title>
-         <title className='tag'>#{arr.shift()}</title>
+         <title className='tag'>#{arr[0]}</title>
+         <title className='tag'>#{arr[1]}</title>
+         <title className='tag'>#{arr[2]}</title>
+         <title className='tag'>#{arr[3]}</title>
     </div>
    );  
+
+
 };
 
 export default Tags;
