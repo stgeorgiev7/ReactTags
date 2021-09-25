b8eabe3d49466cc1e6e440a0fcb3c8c92dbe7850
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-09-25 18:37:35 +0300

    made ul with list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Tags from "./Tags";
+import Tags from './Tags.js';
 
 function App() {
   return (
--- src/Tags.js
+++ src/Tags.js
@@ -3,12 +3,20 @@ import react from "react";
 const Tags = () => {
     const arr = ['boomdotdev', 'task', 'tags', 'react'];
     
-    return (<div className='tags'>
-         <title className='tag'>#{arr[0]}</title>
-         <title className='tag'>#{arr[1]}</title>
-         <title className='tag'>#{arr[2]}</title>
-         <title className='tag'>#{arr[3]}</title>
-    </div>
+    return (<ul className='tags'>
+        <li>
+            <title className='tag'>#{arr[0]}</title>
+        </li>
+        <li>
+            <title className='tag'>#{arr[1]}</title>
+        </li>
+        <li>
+            <title className='tag'>#{arr[2]}</title>
+        </li>
+        <li>
+            <title className='tag'>#{arr[3]}</title>
+        </li>
+        </ul>
    );  
 
 
